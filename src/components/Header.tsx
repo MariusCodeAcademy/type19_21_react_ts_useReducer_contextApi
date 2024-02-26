7ae1c4b8c29924a701b27f4c951b267d78b1f915
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-26 08:41:28 +0200

    Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,16 @@ import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header>
-      <Link to={'/'}>Logo</Link>
-      <nav>
-        <NavLink to={'/'}>Home</NavLink>
-        <NavLink to={'/counters'}>Counters</NavLink>
-      </nav>
+    <header className='header'>
+      <div className='container flex'>
+        <Link className='logo' to={'/'}>
+          Logo
+        </Link>
+        <nav className='main-nav'>
+          <NavLink to={'/'}>Home</NavLink>
+          <NavLink to={'/counters'}>Counters</NavLink>
+        </nav>
+      </div>
     </header>
   );
 }
