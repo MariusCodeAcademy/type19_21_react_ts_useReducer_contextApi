1b3cf99197e5eb5ee62060a327f72121870ed12e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-26 10:38:38 +0200

    create context

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,14 @@
 export default function LoginPage() {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log('pateikta forma');
+  }
+
   return (
     <div className='container'>
       <h1>LoginPage</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input className='input' type='email' placeholder='email' />
         <input className='input' type='password' placeholder='Pass' />
         <button type='submit'>Login</button>
---
+++ src/store/AuthProvider.tsx
@@ -0,0 +1,35 @@
+import { createContext } from 'react';
+
+type CtxType = {
+  login(email: string): void;
+  logout(): void;
+  email: string;
+};
+
+const AuthContext = createContext({
+  login(email: string) {},
+  logout() {},
+  email: '',
+});
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+};
+
+export default function AuthProvider({ children }: AuthProviderProps) {
+  function login() {
+    console.log('login ===');
+  }
+
+  function logout() {
+    //
+  }
+
+  const contextValue: CtxType = {
+    login,
+    logout,
+    email: '',
+  };
+
+  return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
+}
