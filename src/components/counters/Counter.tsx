0c7639e6dd509ce3f376e13f9032ed50d78bee4e
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-26 08:47:11 +0200

    counter cmp jsx

---
+++ src/components/counters/Counter.tsx
@@ -0,0 +1,14 @@
+export default function Counter() {
+  return (
+    <div className='card'>
+      <h2>Counter</h2>
+
+      <p>value</p>
+
+      <div className='control'>
+        <button>Up</button>
+        <button>Down</button>
+      </div>
+    </div>
+  );
+}
