8c3a8007e4098f753d45e5c82a060d7073c760a8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-02-26 08:19:47 +0200

    empty pages

---
+++ src/pages/CountersPage.tsx
@@ -0,0 +1,13 @@
+export default function CountersPage() {
+  return (
+    <div className='container'>
+      <h1>CountersPage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sed qui tenetur maxime
+        repellendus, aliquid, voluptatum maiores rerum ipsa optio iusto alias quo culpa animi,
+        perspiciatis sapiente harum vitae nostrum.
+      </p>
+      <button>click me</button>
+    </div>
+  );
+}
